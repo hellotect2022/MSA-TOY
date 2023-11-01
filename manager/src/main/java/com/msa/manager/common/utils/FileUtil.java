0ae70558bc6a2439c50d4898dff7583898721c04
dhhan <sd>
2023-11-01 20:43:34 +0900

    filedownload

--- manager/src/main/java/com/msa/manager/common/utils/FileUtil.java
+++ manager/src/main/java/com/msa/manager/common/utils/FileUtil.java
@@ -1,27 +1,28 @@
 package com.msa.manager.common.utils;
 
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FileUtil {
 
     public static String UPLOAD_DIR = "C:/TEST/";
     public static String fileUpload(MultipartFile file, String dirPath){
         try{
-            byte[] utf8Bytes = file.getOriginalFilename().getBytes(StandardCharsets.UTF_8);
-            String fileName = new String(utf8Bytes, StandardCharsets.UTF_8.toString());
-            String encodedFileName = URLEncoder.encode(file.getOriginalFilename(), StandardCharsets.UTF_8.toString())
-                    .replace("+", "%20");
-            System.out.println("getOriginalFilename:::"+file.getOriginalFilename());
-            System.out.println("utf8Bytes:::"+utf8Bytes);
-            System.out.println("encodedFileName:::"+encodedFileName);
-
+            String fileName = file.getOriginalFilename();
             String filePath = UPLOAD_DIR+fileName;
             File dirFile = new File(UPLOAD_DIR);
-
 
             if(!dirFile.exists())
             {
@@ -38,6 +39,40 @@ public class FileUtil {
         }catch (Exception e){
             return "File download error"+e.toString();
         }
+    }
 
+    public static ResponseEntity fileDownload(String fileName) throws IOException {
+        String filePath = fileName;
+        System.out.println("filePath:::"+filePath);
+        File file = new File(filePath);
+        System.out.println("fileName:::"+file.getName());
+
+        // 파일이 존재하는지 확인하고 InputStreamResource로 변환
+        InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+
+        // 다운로드할 때 파일명 설정
+        HttpHeaders headers = new HttpHeaders();
+        String encodedFileName = URLEncoder.encode(file.getName(), StandardCharsets.UTF_8.toString());
+
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + encodedFileName);
+
+        // 파일의 MIME 타입에 따라 Content-Type 설정
+        String contentType = "application/octet-stream"; // 기본적으로는 이진 파일로 설정
+        Path path = Paths.get(filePath);
+        if (path != null) {
+            contentType = Files.probeContentType(path);
+        }
+
+        // ResponseEntity를 이용하여 파일 다운로드 응답 생성
+        return ResponseEntity.ok()
+                .headers(headers)
+                .contentLength(file.length())
+                .contentType(MediaType.parseMediaType(contentType))
+                .body(resource);
     }
 }
+
+
+
+
+
--- manager/src/main/java/com/msa/manager/controller/api/TestApiController.java
+++ manager/src/main/java/com/msa/manager/controller/api/TestApiController.java
@@ -2,10 +2,14 @@ package com.msa.manager.controller.api;
 
 import com.msa.manager.common.utils.FileUtil;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Controller
 @EnableAutoConfiguration
@@ -20,8 +24,15 @@ public class TestApiController {
     @RequestMapping("/fileupload")
     @ResponseBody
     public String file_upload(MultipartFile file) {
-        int test = 1;
         String result = FileUtil.fileUpload(file,"test");
+        return result;
+    }
+
+    @RequestMapping("/filedownload")
+    @ResponseBody
+    public ResponseEntity file_download(@RequestParam("fileName") String fileName) throws IOException {
+        String filePath = "C:/TEST/" + fileName;
+        ResponseEntity result = FileUtil.fileDownload(filePath);
         return result;
     }
 }
